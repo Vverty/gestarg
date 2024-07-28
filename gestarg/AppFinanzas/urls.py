3cc5f98e524770a5b9afadbcd7dd0132521bf378
Felipe Lorenzo <lorenzofelipe7@gmail.com>
2024-07-28 01:17:37 -0300

    Agrego form

--- gestarg/AppFinanzas/urls.py
+++ gestarg/AppFinanzas/urls.py
@@ -4,12 +4,12 @@ from AppFinanzas import views
 
 urlpatterns = [
     path('', views.inicio, name='Inicio'),
-    path('agregar-gasto/', views.agregar_gasto, name='AgregarGasto'),
-    path('mostrar-gastos/', views.mostrar_gastos, name='MostrarGastos'),
-    path('editar-gasto/', views.editar_gasto, name='EditarGastos'),
-    path('eliminar-gasto/', views.eliminar_gasto, name='ElimarGasto'),
+    path('gastos/', views.mostrar_gastos, name='MostrarGastos'),
+    path('gastos/agregar/', views.agregar_gasto, name='AgregarGasto'),
+    path('gastos/editar/<int:id>/', views.editar_gasto, name='EditarGasto'),
+    path('gastos/eliminar/<int:id>/', views.eliminar_gasto, name='EliminarGasto'),
     path('agregar-ingreso/', views.agregar_ingreso, name='AgregarIngreso'),
     path('mostrar-ingresos/', views.mostrar_ingresos, name='MostrarIngresos'),
-    path('editar-ingreso/', views.editar_ingreso, name='EditarIngresos'),
-    path('eliminar-ingreso/', views.eliminar_ingreso, name='ElimarIngreso')
+    path('editar-ingreso/', views.editar_ingreso, name='EditarIngreso'),
+    path('eliminar-ingreso/', views.eliminar_ingreso, name='EliminarIngreso')
 ]
--- gestarg/AppFinanzas/views.py
+++ gestarg/AppFinanzas/views.py
@@ -1,21 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from .models import Gasto
+from .forms import GastoForm
 
 def inicio(request):
     return render(request, 'AppFinanzas/index.html')
 
 #GASTOS
 def agregar_gasto(request):
-    return render(request, 'AppFinanzas/agregar_gasto.html')
+    if request.method == 'POST':
+        form = GastoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('MostrarGastos')
+    else:
+        form = GastoForm()
+    return render(request, 'AppFinanzas/agregar_gasto.html', {'form': form})
 
 def mostrar_gastos(request):
-    return render(request, 'AppFinanzas/mostrar_gastos.html')
+    gastos = Gasto.objects.all()  
+    context = {
+        'gastos': gastos,  
+    }
+    return render(request, 'AppFinanzas/mostrar_gastos.html', context)
 
-def editar_gasto(request):
-    return render(request, 'AppFinanzas/editar_gasto.html')
+def editar_gasto(request, id):
+    gasto = get_object_or_404(Gasto, id=id)
+    if request.method == 'POST':
+        form = GastoForm(request.POST, instance=gasto)
+        if form.is_valid():
+            form.save()
+            return redirect('MostrarGastos')
+    else:
+        form = GastoForm(instance=gasto)
+    return render(request, 'AppFinanzas/editar_gasto.html', {'form': form})
 
-def eliminar_gasto(request):
-    return render(request, 'AppFinanzas/eliminar_gasto.html')
+def eliminar_gasto(request, id):
+    gasto = get_object_or_404(Gasto, id=id)
+    if request.method == 'POST':
+        gasto.delete()
+        return redirect('MostrarGastos')
+    return render(request, 'AppFinanzas/eliminar_gasto.html', {'gasto': gasto})
 
 #INGRESOS
 def agregar_ingreso(request):
