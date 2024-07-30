1ced49a7427721d49b51a371f484546ae1e99fcd
Felipe Lorenzo <lorenzofelipe7@gmail.com>
2024-07-30 17:49:16 -0300

    Avanzando con Users

--- gestarg/Users/urls.py
+++ gestarg/Users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from Users import views
 
 urlpatterns = [
-    path()
+    path('login/', views.login_request, Name='Login'),
+    path('register/', views.register, Name='Register')
 ]
--- gestarg/gestarg/urls.py
+++ gestarg/gestarg/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('AppFinanzas/', include('AppFinanzas.urls')),
+    path('', include('AppFinanzas.urls')),
     path('Users/', include('Users.urls'))
 ]
