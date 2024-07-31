8a88d5c57ce3e347f13ed9df62803d10e1951c0b
Felipe Lorenzo <lorenzofelipe7@gmail.com>
2024-07-31 00:30:49 -0300

    agregando notas para seguir trabajando

--- gestarg/gestarg/urls.py
+++ gestarg/gestarg/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('AppFinanzas.urls')),
-    path('Users/', include('Users.urls'))
+    path('users/', include('Users.urls'))
 ]
