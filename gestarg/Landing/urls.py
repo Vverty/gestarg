a25e507825bd00d3f6dc2b1dc478fd5b079bb953
Felipe Lorenzo <lorenzofelipe7@gmail.com>
2024-08-09 18:25:04 -0300

    Agregando Acerca De Mí

--- gestarg/Landing/urls.py
+++ gestarg/Landing/urls.py
@@ -4,4 +4,5 @@ from Landing import views
 urlpatterns = [
     path('nosotros/', views.nosotros, name='Nosotros'),
     path('', views.inicio_landing, name='InicioLanding'),
+    path('acerca-de-mi', views.acerca_de_mi, name='AboutMe'),
 ]
--- gestarg/Landing/views.py
+++ gestarg/Landing/views.py
@@ -7,3 +7,6 @@ def nosotros(request):
 def inicio_landing(request):
     current_year = datetime.now().year
     return render(request, 'Landing/index.html', {'current_year': current_year})
+
+def acerca_de_mi(request):
+    return render(request, 'Landing/about_me.html')
