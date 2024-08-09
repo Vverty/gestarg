0d1ee716a7b2fdd3d8c09b06cb2b531f764c8ef0
Felipe Lorenzo <lorenzofelipe7@gmail.com>
2024-08-09 18:33:36 -0300

    emprolijando urls

--- gestarg/Landing/urls.py
+++ gestarg/Landing/urls.py
@@ -4,5 +4,5 @@ from Landing import views
 urlpatterns = [
     path('nosotros/', views.nosotros, name='Nosotros'),
     path('', views.inicio_landing, name='InicioLanding'),
-    path('acerca-de-mi', views.acerca_de_mi, name='AboutMe'),
+    path('acerca-de-mi/', views.acerca_de_mi, name='AboutMe')
 ]
